1934a31d74999c30742d623226affb3faf856fd6
sungminkim <sungminkim@192.168.0.10>
2017-09-22 01:48:21 +0900

    fixed candy box

--- src/algorithm/candy_box.java
+++ src/algorithm/candy_box.java
@@ -1,56 +1,83 @@
 package algorithm;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class candy_box {
-	static int n = 8;
-	static int tree_depth = (int) (Math.log(n) / Math.log(2));
-	static int tree_size = (1 << (tree_depth + 1));
+	static int MAX = 100000;
+	static int tree_depth = (int) (Math.log(MAX) / Math.log(2));
+	static int tree_size = (1 << (tree_depth + 2));
 	
-	static long list[] = new long[n];
-	static long tree[] = new long[tree_size];
+	static int list[] = new int[MAX];
+	static int tree[] = new int[tree_size];
 	
-	public static void main(String[] args) {
-		
-		
-		for (int i = 0; i < n; i++) {
-			list[i] = 0;
-		}
+	public static void main(String[] args) throws IOException {
 //		System.out.println("treedepth: " + tree_depth);
 //		System.out.println("treesize: " + tree_size);
-		init(list, tree, 0, 0, n-1);
 		
+		System.setIn(new FileInputStream("candy_box_input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		
-		
-		for (int i = 0; i < n; i++) {
-			System.out.print(list[i]);
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+			String list[] = line.split(" ");
+			
+			
+			int action = Integer.parseInt(list[0]);
+			
+//			System.out.println("action: " + action);
+			if (action == 1) {
+				int order = Integer.parseInt(list[1]);
+//				System.out.println("order: " + order);
+				
+				int index = pop(tree, 1, 0, MAX-1, order);
+				System.out.println(index);
+				update(tree, 1, 0, MAX-1, index, -1);
+			} else {
+				int index = Integer.parseInt(list[1]);
+				int count = Integer.parseInt(list[2]);
+//				System.out.println("index: " + index);
+//				System.out.println("count: " + count);
+				
+				update(tree, 1, 0, MAX-1, index, count);
+			}
 		}
-		System.out.println("");
-		for (int i = 0; i < tree_size; i++) {
-			System.out.print(tree[i]);
-			System.out.print(" ");
-		}
-		System.out.println("");
+		
+//		init(list, tree, 0, 0, n-1);
+		
+//		System.out.println("");
+//		for (int i = 0; i < tree_size; i++) {
+//			System.out.print(tree[i]);
+//			System.out.print(" ");
+//		}
+//		System.out.println("");
 		
 	}
-	
-	public static int pop(int index) {
-		return 0;
-	}
-	
-	public static void put(int order, int count) {
-		list[order] = list[order] + count;
-	}
-	
-	public static long init(long list[], long tree[], int node, int start, int end) {
+
+	public static int pop(int[] tree, int node, int start, int end, int order) {
 		if (start == end) {
-			return tree[node] = list[start];
+			return start;
 		}
 		
 		int mid = (start + end) / 2;
 		
-		return tree[node] = init(list, tree, node * 2, start, mid) + init(list, tree, node * 2 + 1, mid + 1, end);
+		if (node * 2 <= tree_size && tree[node * 2] >= order) {
+			return pop(tree, node * 2, start, mid, order);
+		}
+		
+		if (node * 2 + 1 <= tree_size && tree[node * 2 + 1] >= order) {
+			return pop(tree, node * 2 + 1, mid + 1, end, order);
+		}
+		return 0;
 	}
 	
-	public static void update(long list[], long tree[], int node, int start, int end, int index, long diff) {
+	public static void update(int tree[], int node, int start, int end, int index, int diff) {
+//		System.out.println("start: " + start);
+//		System.out.println("end: " + end);
+//		System.out.println("node: " + node);
 		if (!(start <= index && index <= end)) {
 			return;
 		}
@@ -59,8 +86,9 @@ public class candy_box {
 		
 		if (start != end) {
 			int mid = (start + end) / 2;
-			update(list, tree, node * 2, start, mid, index, diff);
-			update(list, tree, node * 2 + 1, mid + 1, end, index, diff);
+			update(tree, node * 2, start, mid, index, diff);
+			update(tree, node * 2 + 1, mid + 1, end, index, diff);
 		}
+		return;
 	}
 }
